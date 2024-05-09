2e9e51fcaa53b20dff262efda090cc3d8c974c18
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-05-10 10:12:03 +1200

    router update with walk component

--- client/router.tsx
+++ client/router.tsx
@@ -6,13 +6,13 @@ import {
 } from 'react-router-dom'
 import App from './components/App'
 import Home from './components/Home'
-import Greatwalks from './components/Greatwalks.tsx'
+import Walk from './components/Walks.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path="/" element={<App />} />,
     <Route index element={<Home />} />,
-    <Route path="greatwalks/:greatwalksId" element={<GreatWalks />} />,
+    <Route path="Walk/:WalkId" element={<Walk />} />,
   ])
 )
 
