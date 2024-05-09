e268ec1d5ed29ca7f7b53d0e8db02c3a1c3a7904
Nathan Emmerson <tiopira98@gmail.com>
2024-05-10 09:48:13 +1200

    added app component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,43 @@
 import Nav from './Nav'
 
+import { useState } from 'react'
+// import data from ''
+
 function App() {
+  const [state, setState] = useState('Select') // Fixed typo
+
+  const handleNext = () => {
+    // Add logic for next button functionality
+    // loop throw array
+    // shuffle throw the array
+  }
+
+  const handleBack = () => {
+    // Add logic for back button functionality
+  }
+
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <h1>Welcome to our App</h1>
+      <p className="fronttitle">
+        You can check out all the best walks in New Zealand
+      </p>
+      <img
+        className="mainheader"
+        alt="nz best place"
+        src="/images/.png" // Add correct image source
+      />
+      <li></li>
+
+      <button className="button" onClick={handleBack}>
+        Back
+      </button>
+      <button className="button" onClick={handleNext}>
+        Next
+      </button>
     </div>
   )
 }
 
 export default App
+
