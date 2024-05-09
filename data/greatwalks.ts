2d4dc15ac53c19fe149eac755cef1197aa37b2e3
Bihui Sun <bihui.sun1107@gmail.com>
2024-05-09 21:25:00 +1200

    finish greatwalks data

--- data/greatwalks.ts
+++ data/greatwalks.ts
@@ -1,5 +1,6 @@
 export default [
   {
+    code: 'AT',
     name: 'Abel Tasman Coast Track',
     region: 'South Island, Nelson/Tasman region',
     distance: '60km one way',
@@ -9,6 +10,7 @@ export default [
       'Blessed with a mild climate, golden beaches and lush coastal native bush, the Abel Tasman Coast Track has it all. Choose to hike the whole track, water taxi between different locations or kayak from Mārahau to campsites in the southern section of the track at any time of the year.',
   },
   {
+    code: 'HT',
     name: 'Heaphy Track',
     region: 'South Island, Nelson/Tasman and West Coast regions',
     distance: '78.4km one way',
@@ -16,5 +18,85 @@ export default [
     difficultyRating: 'Intermediate',
     description:
       "If it's varied and rugged landscapes with mountain views you're looking for, youll be spoilt for choice on the Heaphy Track. Travel through expansive tussock downs, lush forests and nīkau palms to the roaring seas of the West Coast.",
+  },
+  {
+    code: 'KT',
+    name: 'Kepler Track',
+    region: 'South Island, Fiordland region',
+    distance: '60km loop',
+    duration: '3-4 days',
+    difficultyRating: 'Intermediate',
+    description:
+      'Set out on a wilderness adventure above the clouds, high in the mountains above Lakes Te Anau and Manapouri in Te Wāhipounamu - South West New Zealand World Heritage Area. The drama of these vast tussock-covered ridgelines and spectacular alpine vistas contrasts with the peaceful lakes and the beech forest of the Iris Burn valley.',
+  },
+  {
+    code: 'MT',
+    name: 'Milford Track',
+    region: 'South Island, Fiordland region',
+    distance: '53.5km loop',
+    duration: '4 days',
+    difficultyRating: 'Intermediate',
+    description:
+      "Experience the 'finest walk in the world' as you retrace the steps of early explorers on the worldrenowned Milford Track. Take a journey along valleys carved by glaciers, wander through ancient rainforests and admire cascading waterfalls. You'll follow a historic route through Fiordland National Park, part of Te Wāhipounamu - South West New Zealand World Heritage Area, before reaching your final destination, the spectacular Milford Sound/Piopiotahi. ",
+  },
+  {
+    code: 'PT',
+    name: 'Paparoa Track',
+    region: 'South Island, West Coast region',
+    distance: '55km one way',
+    duration: '3 days one way',
+    difficultyRating: 'Intermediate',
+    description:
+      'Experience our first purposebuilt, shared-use hiking and mountain biking Great Walk in the Paparoa National Park on the West Coast of the South Island. This Great Walk crosses the Paparoa Range, taking you through alpine tops, limestone karst landscapes and thriving rainforests. The Pike29 Memorial Track climbs from the site of the former Pike River mine to the junction with the Paparoa Track.',
+  },
+  {
+    code: 'RT',
+    name: 'Rakiura Track',
+    region: 'Stewart Island/Rakiura, Southland region',
+    distance: '32km loop',
+    duration: '3 days',
+    difficultyRating: 'Intermediate',
+    description:
+      "Located in Rakiura National Park on Stewart Island/Rakiura, this is a leisurely hike suitable for anyone with a moderate level of fitness. Relax and unwind in the peaceful surroundings, with the bush, birds and beach at your side. At night, you'll enjoy world-class star-gazing. Stewart Island/Rakiura is the world's fifth International Dark Sky Sanctuary. If you're lucky, you might get to see the night sky glow red and green with the aurora australis/southern lights. During the winter months, the island’s weather is cool yet settled, making the Rakiura Track a great winter escape. This is a journey that links the past with the present - Stewart Island/Rakiura is steeped in history.",
+  },
+  {
+    code: 'RBT',
+    name: 'Routeburn Track',
+    region: 'South Island, Otago and Fiordland regions',
+    distance: '33km one way',
+    duration: '2-4 days',
+    difficultyRating: 'Intermediate',
+    description:
+      "The Routeburn Track is the ultimate alpine adventure, taking you through ice-carved valleys and below the majestic peaks of the Southern Alps/Kā Tiritiri o te Moana. A short trip from bustling Queenstown, this Great Walk links Mount Aspiring and Fiordland National Parks in Te Wāhipounamu - South West New Zealand World Heritage Area. Weaving through meadows, reflective tarns and alpine gardens, you'll be rewarded with spectacular vistas over vast mountain ranges and valleys.",
+  },
+  {
+    code: 'LWT',
+    name: 'Lake Waikaremoana Track',
+    region: 'Nouth Island, East Coast region',
+    distance: '46km one way',
+    duration: '3-4 days',
+    difficultyRating: 'Intermediate',
+    description:
+      "Lake Waikaremoana is a Great Walk that is more of a backcountry, off the beaten track experience. Be immersed into stunning natural wilderness and welcomed into the homeland of Ngai Tūhoe. Te Urewera is recognised in New Zealand law as a living person. Te Urewera is spoken for and governed by a board. Care for Te Urewera, including the tracks and facilities, is carried out by Te Uru Taumatua - Ngāi Tūhoe's operational entity.",
+  },
+  {
+    code: 'TNC',
+    name: 'Tongariro Northern Circuit',
+    region: 'Nouth Island, Central North Island region',
+    distance: '44.9km one way',
+    duration: '3-4 days',
+    difficultyRating: 'Intermediate',
+    description:
+      "This World Heritage site is a landscape of stark contrasts. You'll pass alpine vegetation, lush forest, tranquil lakes and desert-like plateaux, with amazing views at every turn. As the circuit winds past Mount Tongariro and Mount Ngāuruhoe, you will be dazzled by dramatic volcanic landscapes and New Zealand's rich geological and ancestral past.",
+  },
+  {
+    code: 'WJ',
+    name: 'Whanganui Journey',
+    region: 'Nouth Island, Manawatu/Whanganui region',
+    distance: '87 or 145km one way',
+    duration: 'Kayaking and canoeing 3 or 5 days',
+    difficultyRating: 'Intermediate',
+    description:
+      "Paddle through a landscape of towering cliffs and deep valleys on the magical Whanganui Journey. You'll travel through calm waters and foaming rapids, on an unforgettable trip into the heart of the Whanganui National Park. Experience the beauty, history and culture of the mighty Whanganui River in the south-west of the North Island. Choose a 5-day journey from Taumarunui to Pipiriki (145 km), or a shorter, 3-day journey starting at Whakahoro (88 km), featuring the most spectacular stretches of the river. In 2017 the Whanganui River was legally recognised as Te Awa Tupua, a living and indivisible whole from the mountains to the sea, its tributaries and all its physical and metaphysical elements. Due care and respect to the river at all times has always been a key expectation of tāngata whenua and the new legal status reinforces this.",
   },
 ]
