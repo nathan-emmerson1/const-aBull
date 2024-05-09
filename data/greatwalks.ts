a08025ba88b2825a2a716bdd5519da63a40cd3ba
Bihui Sun <bihui.sun1107@gmail.com>
2024-05-09 17:28:35 +1200

    add data for greatwalks

---
+++ data/greatwalks.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: 'Abel Tasman Coast Track',
+    region: 'South Island, Nelson/Tasman region',
+    distance: '60km one way',
+    duration: '3-5 days one way',
+    difficultyRating: 'Intermediate',
+    desctiption:
+      'Blessed with a mild climate, golden beaches and lush coastal native bush, the Abel Tasman Coast Track has it all. Choose to hike the whole track, water taxi between different locations or kayak from Mārahau to campsites in the southern section of the track at any time of the year.',
+  },
+  {
+    name: 'Heaphy Track',
+    region: 'South Island, Nelson/Tasman and West Coast regions',
+    distance: '78.4km one way',
+    duration: '4-6 days',
+    difficultyRating: 'Intermediate',
+    description:
+      "If it's varied and rugged landscapes with mountain views you're looking for, youll be spoilt for choice on the Heaphy Track. Travel through expansive tussock downs, lush forests and nīkau palms to the roaring seas of the West Coast.",
+  },
+]
